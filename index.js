b8f9b0d19c9bae6c5706ead38ced89965b7c90fe
monirakibul <monirakibul@gmail.com>
2022-05-23 22:57:37 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ekxfy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+client.connect(err => {
+    const collection = client.db("bikebackup").collection("users");
+    // perform actions on the collection object
+    console.log('con')
+})
+
 app.get('/', (req, res) => {
     res.send('running')
 })
