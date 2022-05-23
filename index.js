5a5b8705856cc06656588d974f5a37201c958e89
monirakibul <monirakibul@gmail.com>
2022-05-24 02:31:46 +0600

    review insert and find added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -10,16 +11,49 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ekxfy.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-client.connect(err => {
-    const collection = client.db("bikebackup").collection("users");
-    // perform actions on the collection object
-    console.log('con')
-})
+async function run() {
+    try {
+        await client.connect()
+        const productsCollection = client.db('BikeBackup').collection('products')
+        const reviewsCollection = client.db('BikeBackup').collection('reviews')
+
+
+        // product 
+        app.get('/products', async (req, res) => {
+            const query = {}
+            const cursor = productsCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        });
+        // add product 
+        app.post('/product', async (req, res) => {
+            const product = req.body;
+            const result = productsCollection.insertOne(product)
+            res.send(result)
+        })
+
+        // get review
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewsCollection.find(query)
+            const reviews = await cursor.toArray()
+            res.send(reviews)
+        });
+        // add review 
+        app.post('/review', async (req, res) => {
+            const review = req.body;
+            const result = reviewsCollection.insertOne(review)
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('running')
